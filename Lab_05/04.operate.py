ddd19d10e9bc2594f9115b0669aef6e49bdce975
pavel-stoykov <pavel.stoykov@students.softuni.bg>
2020-10-02 21:06:56 +0300

    remove old code

--- Lab_05/04.operate.py
+++ Lab_05/04.operate.py
@@ -8,14 +8,6 @@ def operate(op, *args):
         "/": lambda a, b: a / b
     }
     return reduce(operators_dict[op], args)
-    # if op == "+":
-    #    return operators_dict['+']
-    # elif op == '-':
-    #     return operators_dict['-']
-    # elif op == '*':
-    #     return operators_dict['*']
-    # elif op == '/':
-    #     return operators_dict['/']
 
 
 print(operate("+", 1, 2, 3))
