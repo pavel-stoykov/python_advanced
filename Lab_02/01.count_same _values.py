7adc27715048107d7928c1b140f92a25fde712ed
pavel-stoykov <pavel.stoykov@students.softuni.bg>
2020-10-03 18:07:18 +0300

    solution without defaultdict

--- Lab_02/01.count_same _values.py
+++ Lab_02/01.count_same _values.py
@@ -1,10 +1,13 @@
-from collections import defaultdict
+# from collections import defaultdict
 
 list_of_numbers = map(float, input().split())
 
-nums_dict = defaultdict(int)
+# nums_dict = defaultdict(int)
+nums_dict = {}
 
 for num in list_of_numbers:
+    if num not in nums_dict:
+        nums_dict[num] = 0
     nums_dict[num] += 1
 
 for number, counter in nums_dict.items():
