ff63d8b270bf350e29f91f48c7974cbea7e755a4
pavel-stoykov <pavel.stoykov@students.softuni.bg>
2020-10-11 10:53:38 +0300

    refactor

--- EXAM(OLD PROBLEMS)/01.paint_colors.py
+++ EXAM(OLD PROBLEMS)/01.paint_colors.py
@@ -1,36 +1,38 @@
-from collections import deque
-from itertools import product
+string = input().split()
 
 main_colors = ['red', 'yellow', 'blue']
-seconadary_colors = ['orange', 'purple', 'green'] 
-
-substring = deque(input().split())
+secondary_colors = {
+    'orange': ('red', 'yellow'),
+    'purple': ('red', 'blue'),
+    'green': ('yellow', 'blue')
+}
 
 find_colors = []
 
-while substring:
-    current_color = []
-    first_substring = substring.popleft()
-    if substring:
-        last_substring = substring.pop()
-        current_color = [first_substring + last_substring, last_substring + first_substring]
+while string:
+    first_substring = string.pop(0)
+    if string:
+        last_substring = string.pop()
     else:
-        color = first_substring
+        last_substring = ''
 
-    for color in current_color:
-        if color in seconadary_colors:
-            if color == 'orange' and 'red' in find_colors and 'yellow' in find_colors:
-                find_colors.append(color)
-            elif color == 'purple' and 'red' in find_colors and 'blue' in find_colors:
-                find_colors.append(color)
-            elif color == 'green' and 'blue' in find_colors and 'yellow' in find_colors:
-                find_colors.append(color)
-        if color in main_colors:
-            find_colors.append(color)
-            break
-    # first_substring = first_substring[:-1]
-    # last_substring = last_substring[:-1]
-    # color = first_substring + last_substring
-    # middle = len(substring) // 2
-    # substring.insert(middle, color)
+    first_string = first_substring + last_substring
+    second_string = last_substring + first_substring
+
+    if first_string in main_colors or first_string in secondary_colors:
+        find_colors.append(first_string)
+    elif second_string in main_colors or second_string in secondary_colors:
+        find_colors.append(second_string)
+    else:
+        middle_of_string = len(string) // 2
+        if len(first_substring) > 1:
+            first_substring = first_substring[:-1]
+            string.insert(middle_of_string, first_substring)
+        if len(last_substring) > 1:
+            last_substring = last_substring[:-1]
+            string.insert(middle_of_string, last_substring)
+for color in find_colors:
+    if color in secondary_colors:
+        if any(x not in find_colors for x in secondary_colors[color]):
+            find_colors.remove(color)
 print(find_colors)
--- demo.py
+++ demo.py
@@ -1,5 +1,15 @@
+import collections
+import itertools
 
-word = 'abcba'
-rev_word = word[::-1]
+substring = collections.deque(input().split())
 
-print(rev_word)
+find_colors = []
+
+while substring:
+    current_color = []
+    first_substring = substring.popleft()
+    if substring:
+        last_substring = substring.pop()
+    current_color = [first_substring + last_substring, last_substring + first_substring]
+    for color in current_color:
+        if color in 
